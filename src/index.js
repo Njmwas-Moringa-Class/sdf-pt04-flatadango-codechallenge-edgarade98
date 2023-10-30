8924d660ad679777ddc16ad2169b2d6d1cff116e
Edgar <eddyade98@gmail.com>
2023-10-30 15:00:24 +0300

    Completed CodeChallenge

--- src/index.js
+++ src/index.js
@@ -1,47 +1,80 @@
-// Your code here
+// your code here
 
 //See the first movie's details, including its poster, title, runtime, showtime, and available tickets when the page loads.
-
-fetch('http://localhost:3000/films/1')
-  .then(response => response.json())
-  .then(filmsData => {
-    console.log('Films:', filmsData);
+function filmsArray(){
+  fetch("http://localhost:3000/films")
+  .then(res=>res.json())
+  .then(filmsArray=>{
+      displayMovieTitles(filmsArray)
+      displayMovieOne(filmsArray[0])
   })
+}
+filmsArray()
 
-function renderOneFilm (film) {
-   let movieList = document.createElement('li')
-   list.classname = 'film item'
-   list.innerHTML = `
-   <li>${film.title}</li>
-   `
-   let moviePoster = document.createElement('img')
-    image.id = 'poster'
-    image.innerHTML = `
-    <img ${film.poster} />
-    `
-
-   let movieDescription = document.createElement('div')
-   card.classname = 'card'
-   card.innerHTML = 
-   `<div class = "card">
-   <h4>${film.title}</h4>
-   <p>${film.runtime}</p>
-   </div>
-   <div class = "content">
-   <p>${film.descripiton}</p>
-   <span class = "ui label">${film.showtime}</span>
-   <span>${film.tickets_sold}</span>
-   </div>
-   <div>
-   <button> Buy Ticket </button>
-   </div>
-   `
-  document.querySelector('#showing').appendChild(movieDescription)
-  document.querySelector('#poster').appendChild(moviePoster)
-  document.querySelector('.film.item').appendChild(movieList)
+function displayMovieTitles(filmsarray){
+  let ul=document.getElementById("films")
+  ul.innerHTML= ""  
+  filmsarray.map(film =>{
+      let li=document.createElement("li")
+      li.className="film item"
+      li.addEventListener("click",()=>handleClick(film))
+      li.textContent=`${film.title}`
+      ul.appendChild(li)
+  }) 
 }
 
-function initialize () {
-    filmsData.forEach(film => renderOneFilm(film))
+function displayMovieOne(data){  
+  let h1=document.getElementById("title")
+  h1.textContent=data.title
+  let div=document.getElementById("runtime")
+  div.textContent=`${data.runtime } minutes`
+  let p=document.getElementById("film-info")
+  p.textContent=data.description
+  let span=document.getElementById("showtime")
+  span.textContent=data.showtime
+  let ticketspan=document.getElementById("ticket-num")
+  ticketspan.textContent=data.capacity-data.tickets_sold
+  let img=document.getElementById("poster")
+  img.src=data.poster
+
 }
-initialize()
+
+function handleClick(data){
+  let h1=document.getElementById("title")
+  h1.textContent=data.title
+  let div=document.getElementById("runtime")
+  div.textContent=`${data.runtime } minutes`
+  let p=document.getElementById("film-info")
+  p.textContent=data.description
+  let span=document.getElementById("showtime")
+  span.textContent=data.showtime
+  let ticketspan=document.getElementById("ticket-num")
+  ticketspan.textContent=data.capacity-data.tickets_sold
+  let img=document.getElementById("poster")
+  img.src=data.poster
+ 
+}
+
+//number of available tickets decreasing on the frontend
+
+document.addEventListener("DOMContentLoaded", function() {
+  const ticketCountElement = document.getElementById("ticket-num");
+
+  const buyTicketBtn = document.getElementById("buy-ticket");
+
+  function updateTicketNumber() {
+    ticketCountElement.textContent = availableTickets;
+  }
+
+  buyTicketBtn.addEventListener("click", function() {
+    if (availableTickets > 0) {
+      availableTickets -= 1;
+      updateTicketNumber();
+    }
+    })
+});
+
+
+
+
+
